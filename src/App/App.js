c027492c0e6fc52038d47fc34e4b169cffebf410
Mateusz <matipu@o2.pl>
2021-11-29 00:04:53 +0100

    add panel component

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,40 @@
 import './App.css';
+import Panel from './../Panel/Panel';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
+function chunkArray(myArray, chunk_size){
+  var results = [];
+  while (myArray.length) {
+    results.push(myArray.splice(0, chunk_size));
+  }
+  
+  while(chunk_size > results[results.length-1].length) {
+    results[results.length-1].push("test");
+  }
+  
+  return results;
+}
 
 function App() {
+  const documents = ["Panel", "Panel", "Panel", "Panel", "Panel", "Panel", "Panel"];
+  const size = 3;
+  var splittedDocuments = chunkArray(documents, size);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          
-        </p>
-
-      </header>
+      <Container fluid>
+        {splittedDocuments.map(documents => (
+          <Row>
+            {documents.map(document => (
+              <Col><div class = "panel"><Panel>{document}</Panel></div></Col>
+            ))}
+          </Row>
+        ))}
+      </Container>
     </div>
   );
 }
---
+++ src/Panel/Panel.js
@@ -0,0 +1,11 @@
+import './Panel.css';
+
+function Panel() {
+  return (
+    <div className="Panel">
+      dupa
+    </div>
+  );
+}
+
+export default Panel;
